f50bbfc3ffbc3603f0e548f6f600214af52801a3
Ing Brayan Martinez <brayanmartinez827@gmail.com>
2020-08-04 19:13:57 -0400

    corregir los test unitarios

--- lib/domain/entity/customer.dart
+++ lib/domain/entity/customer.dart
@@ -19,15 +19,15 @@ class Customer {
       this.status, this.createdAt, this.updatedAt);
 
   Customer.fromJson(Map<String, Object> json)
-      : id = json['id'],
-        code = json['code'],
-        name = json['name'],
-        adress = json['adress'],
-        mail = json['mail'],
-        phone = json['phone'],
-        status = json['status'],
-        createdAt = json['createdAt'],
-        updatedAt = json['updatedAt'];
+      : id = json['id'] as int,
+        code = json['code'] as String,
+        name = json['name'] as String,
+        adress = json['adress'] as String,
+        mail = json['mail'] as String,
+        phone = json['phone'] as String,
+        status = json['status'] as String,
+        createdAt = DateTime.parse(json['createdAt'] as String),
+        updatedAt = DateTime.parse(json['updatedAt'] as String);
 
   Map<String, Object> toJson() => {
     'id' : id,
@@ -37,8 +37,8 @@ class Customer {
     'mail' : mail,
     'phone' : phone,
     'status' : status,
-    'createdAt' : createdAt,
-    'updatedAt' : updatedAt
+    'createdAt' : createdAt.toString(),
+    'updatedAt' : updatedAt.toString()
   };
 
   @override
--- test/reflect/customer_test.dart
+++ test/reflect/customer_test.dart
@@ -9,20 +9,17 @@ void main() {
 
     test('Customer Encode', () {
 
-        var co = new Customer.from(1, 'fff', 'brayan', 'valencia', 'brayanmartinez827@gmail.com',
+        Customer co = new Customer.from(1, 'fff', 'brayan', 'valencia', 'brayanmartinez827@gmail.com',
             '+58426649380', 'Y', DateTime.now(), DateTime.now());
 
         print(jsonEncode(co));
-
-        final DateTime parsedDate = co.createdAt;
-        print(parsedDate);
 
         print("Ejecutando pruebas...");
     });
 
     test("Customer Decode", () {
 
-        var co = '{"id":1,"code":"fff","name":"brayan","adress":"valencia","mail":"brayanmartinez827@gmail.com",'
+        String co = '{"id":1,"code":"fff","name":"brayan","adress":"valencia","mail":"brayanmartinez827@gmail.com",'
             '"phone":"+58426649380","status":"Y","createdAt":"2019-12-04 13:30:38.400808",'
             '"updatedAt":"2019-12-04 13:30:38.406860"}';
 
